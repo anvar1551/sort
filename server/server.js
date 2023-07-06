defa3af46e2bef66e3dcd507ad0585fa55945d81
Anvarbek Sharipov <anvarjon1551@gmail.com>
2023-07-06 21:03:59 +0500

    Headers added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const { google } = require("googleapis");
 const app = express();
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://skysorting.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // Конфигурация доступа к Google Sheets API
 const credentials = require("./credentials.json");
 const { client_email, private_key } = credentials;
